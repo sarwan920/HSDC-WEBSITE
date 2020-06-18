f91967d94ad475171b5e98b389522978e5060ad3
Sarwan Nizamani <sarwan.nizamani8@gmail.com>
2020-06-18 08:15:52 -0700

    add css and view files

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express=require('express');
-
+const path=require('path');
 const app=express();
 
 
 app.set('view engine','ejs');
 app.set("views", "views");
 
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get('/',(req,res)=>{
     res.render('index');
