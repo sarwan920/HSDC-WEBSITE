984012452c4827e588aa39b51959e55486e62131
Sarwan Nizamani <sarwan.nizamani8@gmail.com>
2020-06-18 07:48:08 -0700

    initial Commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express=require('express');
+
+const app=express();
+
+
+app.set('view engine','ejs');
+app.set("views", "views");
+
+
+app.get('/',(req,res)=>{
+    res.render('index');
+});
+
+
+app.listen(3000,()=>{
+    console.log("Server Started");
+});
