9f87a97b85acdc686a5c24adfc0dc70239159ec3
Sarwan Nizamani <sarwan.nizamani8@gmail.com>
2020-06-18 16:05:13 -0700

    add corousel and bootstrap with js script

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.set('view engine','ejs');
 app.set("views", "views");
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(__dirname + '/node_modules/bootstrap/dist'));
 
 // app.get('/',(req,res)=>{
 //     res.render('index');
