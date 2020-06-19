a958d581f4fd9465361df661e4c74ae1408397f5
Sarwan Nizamani <sarwan.nizamani8@gmail.com>
2020-06-19 03:14:56 -0700

    add some files

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express=require('express');
 const path=require('path');
 const app=express();
 
+const PORT=process.env.PORT || 3000;
 
 const errorController=require('./controllers/error');
 
@@ -23,6 +24,6 @@ app.use(mainRoute);
 app.use('/',errorController.getError);
 
 
-app.listen(3000,()=>{
+app.listen(PORT,()=>{
     console.log("Server Started");
 });
